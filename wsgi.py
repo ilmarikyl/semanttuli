fec9e2ce5b02dd6578b083025e54c82a7f86f8f7
ilmarikyl <ilmari_kylliainen@hotmail.com>
2022-10-06 20:16:10 +0300

    add info about url change

--- wsgi.py
+++ wsgi.py
@@ -7,6 +7,7 @@ from paste.translogger import TransLogger
 
 if __name__ == "__main__":
     PORT_NUMBER = int(os.environ.get("PORT", 33507))
+    # PORT_NUMBER = int(os.environ.get("PORT", 8080)) # for fly.io
 
     print(f"Starting app in port {PORT_NUMBER}...")
     serve(TransLogger(app), host="0.0.0.0", port=PORT_NUMBER)  # default port is 8080
